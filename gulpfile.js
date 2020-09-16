9fd0cedb3bb8b125354fe959c21c619d010455f0
Michael Glimmerdahl <michael.glimmerdahl@telekalkyl.se>
2020-09-16 09:08:35 +0200

    Added browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ const uglify = require("gulp-uglify-es").default;
 const cleanCSS = require("gulp-clean-css");
 const image = require("gulp-image");
 const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require('browser-sync').create();
 
-// Paths
+// Paths to source files
 const files = {
     htmlPath: "src/**/*.html",
     cssPath: "src/**/*.css",
@@ -14,10 +15,18 @@ const files = {
     imagePath: "src/images/*"
 }
 
+// Paths to pub files
+const pubFiles = {
+    htmlPath: "pub/**/*.html",
+    cssPath: "pub/**/*.css",
+    jsPath: "pub/**/*.js",
+    imagePath: "pub/images/*"
+}
+
 // Copy html files
 function copyHTML() {
     return src(files.htmlPath)
-        .pipe(dest("pub"));
+        .pipe(dest("pub"))
 }
 
 // Concat and minify js files
@@ -28,7 +37,7 @@ function jsTask() {
             .pipe(uglify())
             .pipe(rename("main.min.js"))
         .pipe(sourcemaps.write())
-        .pipe(dest("pub/js"));
+        .pipe(dest("pub/js"))
 }
 
 // Concat and minify css files
@@ -39,20 +48,28 @@ function cssTask() {
             .pipe(cleanCSS())
             .pipe(rename("main.min.css"))
         .pipe(sourcemaps.write())
-        .pipe(dest("pub/css"));
+        .pipe(dest("pub/css"))
 }
 
 // Compress and copy images
 function imageTask() {
     return src(files.imagePath)
         .pipe(image())
-        .pipe(dest("pub/images"));
+        .pipe(dest("pub/images"))
 }
 
-// Watcher
+// Watcher, starting a browser sync server and running task depending on change
 function watchTask() {
-    watch([files.htmlPath, files.jsPath, files.cssPath, files.imagePath], 
-        parallel(copyHTML, jsTask, cssTask, imageTask));
+    browserSync.init({
+        server: {
+            baseDir: "./pub"
+        }
+    });
+    watch(files.htmlPath, copyHTML);
+    watch(files.jsPath, jsTask);
+    watch(files.cssPath, cssTask);
+    watch(files.imagePath, imageTask);
+    watch([pubFiles.htmlPath, pubFiles.jsPath, pubFiles.cssPath, pubFiles.imagePath]).on("change", browserSync.reload);
 }
 
 
